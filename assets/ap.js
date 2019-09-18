82f906677fc2f548369940bf413dd3d79106c066
MattRoger <mrogerphotography@gmail.com>
2019-09-17 20:17:00 -0600

    added button creating function, buttons lose data-sin attr , added some basic styling

--- assets/ap.js
+++ assets/ap.js
@@ -1,29 +1,57 @@
-alert("i work")
+var sin;
+var sins = ["pride", "greed", "wrath", "lust", "envy", "sloth"]
+var button;
+var sinNum=sins.length+1;
+$("#sinnum").append(sinNum)
 
-$("button").on("click", function(){
+
+renderButtons()
+function renderButtons() {
+    $("#buttons").empty()
+    for (var i = 0; i < sins.length; i++) {
+        var button = $("<button>");
+        button.text(sins[i])
+        button.attr("data-sin", sins[i])
+        $("#buttons").append(button)
+        $("#sin-num").append(sinNum);
+        
+        
+    }
+}
+$("button").on("click", function () {
     console.log(this)
 
-    var sin =$(this).attr("data-sin");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" 
-    +sin + 
-    "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";    
+    var sin = $(this).attr("data-sin");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="
+    + sin +
+    "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
     $.ajax({
-        url:queryURL,
-        method:"GET"
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                gifDiv.addClass("gifimg");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height.url);
+                gifImage.attr("alt", "img failed to load");
+                gifDiv.append(p, gifImage);
+                $("#gifs_place").prepend(gifDiv);
+            }
+            
+        });
 })
-.then(function(response){
-    console.log(response);
-    var results=response.data;
-    for(var i=0; i< results.length; i++){
-        var gifDiv = $("<div>");
-        var rating= results[i].rating;
-        var p=$("<p>").text("Rating: "+ rating);
-        var gifImage= $("<img>");
-        gifImage.attr("src", results[i].images.fixed_height.url);
-        gifImage.attr("alt", "img failed to load");
-        gifDiv.append(p, gifImage);
-        $("#gifs_place").prepend(gifDiv);
-    }
-    
-});
+
+$("#add-sin").on("click", function (event) {
+    console.log("click")
+    event.preventDefault();
+    var sinx = $("#sin-new").val().trim();
+    sins.push(sinx);
+    renderButtons()
+    console.log(sins)
 })
