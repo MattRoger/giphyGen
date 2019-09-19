b9318432993377ff650e85050370c02a46e34054
MattRoger <mrogerphotography@gmail.com>
2019-09-19 11:58:01 -0600

    gifs now start and stop

--- assets/ap.js
+++ assets/ap.js
@@ -6,6 +6,7 @@ var sinNum = sins.length + 1;
 
 
 renderButtons()
+
 function renderButtons() {
     $("#buttons").empty()
     for (var i = 0; i < sins.length; i++) {
@@ -22,32 +23,66 @@ $("#buttons").on("click", "button", function () {
     console.log(this)
     var sin = $(this).attr("data-sin");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="
-    + sin + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-    $.ajax({
+        + sin + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function (response) {
-        console.log(response);
-        var results = response.data;
+            console.log(response);
+            var results = response.data;
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
                 gifDiv.addClass("gifimg");
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height.url);
+                var gifImage = $("<img>").attr("data-state", "still").attr("data-animate", results[i].images.fixed_height.url).attr("data-still", results[i].images.fixed_height_still.url)
+                gifImage.attr("src", results[i].images.fixed_height_still.url);
+                // gifImage.attr("data-state","animated", );
+                
+                // gifImage.attr("data-state","still", results[i].images.fixed_height_still.url);
+                
+                
+
                 gifImage.attr("alt", "img failed to load");
                 gifDiv.append(p, gifImage);
                 $("#gifs_place").prepend(gifDiv);
+                
             }
-
+            
         });
-})
-
-$("#add-sin").on("click", function (event) {
-    console.log("click")
-    event.preventDefault();
+    })
+    $("#gifs_place").on("click", "img", function () {
+        var state = $(this).attr("data-state");
+    
+    
+        var stillURL = $(this).attr("data-still");
+        // var stillURL=$(this).attr("src", results[i].images.fixed_height_still.url);
+    
+        var animateURL = $(this).attr("data-animate")
+        // var animateURL=$(this).attr("src", results[i].images.fixed_height.url)
+    
+    
+        if (state === "still") {
+            $(this).attr({
+                "src": animateURL,
+                "data-state": "animate"
+            });
+    
+        } else {
+            $(this).attr({
+                "src": stillURL,
+                "data-state": "still"
+            })
+    
+        };
+    
+    
+    })
+    
+    $("#add-sin").on("click", function (event) {
+        console.log("click")
+        event.preventDefault();
     var sinx = $("#sin-new").val().trim();
     sins.push(sinx);
     renderButtons()
@@ -55,5 +90,4 @@ $("#add-sin").on("click", function (event) {
     $("#sin-new").val("Add A Sin")
     // $("#sin-num").append(sinNum);      
 })
-
 
